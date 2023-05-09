fa5256c06e1bb95b70cdba40c86713164dbecfba
Chidders <64120293+Chiddersss@users.noreply.github.com>
2023-05-09 09:50:05 +0100

    modified:   src/App.js 	new file:   src/common/index.js 	new file:   src/components/register.js 	modified:   src/utils/index.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import './App.css';
 import Login from './components/Login';
+import { getCookie } from './common';
 
 import { useEffect, useState } from 'react'
+import {authCheck} from './utils'
 
 const App = () => {
 
   const [user, setUser] = useState()
 
+  useEffect(() => {
+    let jwt = getCookie("jwt_token")
+    console.log("!!!!!!!!")
+    console.log(jwt)
+    if (cookie != false) {
+      logininWithToken(jwt)
+    }
+
+  }, [])
+
+  const logininWithToken = async (jwt) => {
+    const user = await authCheck(jwt)
+    setUser(user)
+  }
+
   return (
     <div>
+      <Register />
+      <br></br>
+      <br></br>
       <Login newUser = {setUser}/>
       {user 
         ? 
