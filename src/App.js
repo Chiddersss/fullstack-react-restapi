0d7647258c107802ccbd7fe57b13b5123e6abace
Chidders <64120293+Chiddersss@users.noreply.github.com>
2023-05-15 17:09:39 +0100

    modified:   src/App.js 	modified:   src/components/register.js 	modified:   src/utils/index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import './App.css';
-import Login from './components/Login';
-import { getCookie } from './common';
+import React from "react";
+import Login from './components/Login'
+import Register from './components/Register'
 
-import { useEffect, useState } from 'react'
-import {authCheck} from './utils'
+import {useState, useEffect } from 'react'
+
+import {getCookie} from "./common"
+import {authCheck} from "./utils"
 
 const App = () => {
 
-  const [user, setUser] = useState()
+  const [user, setUser] = useState() 
 
-  useEffect(() => {
+  useEffect(()=>{
     let jwt = getCookie("jwt_token")
-    console.log("!!!!!!!!")
+    console.log("!!!!!!!")
     console.log(jwt)
-    if (cookie != false) {
-      logininWithToken(jwt)
+    
+    if (jwt !== false){
+      loginWithToken(jwt)
     }
 
   }, [])
 
-  const logininWithToken = async (jwt) => {
+  const loginWithToken = async (jwt) => {
     const user = await authCheck(jwt)
     setUser(user)
   }
@@ -29,12 +32,12 @@ const App = () => {
       <Register />
       <br></br>
       <br></br>
-      <Login newUser = {setUser}/>
+      <Login newUser={setUser} />
       {user 
-        ? 
-        <h2>Hello Welcome {user} you have logged in</h2>
+        ?
+        <h2>Hello welcome {user} you have logged in</h2>
         :
-        <h2>Please Log in</h2>
+        <h2>Please log in</h2>
       }
     </div>
   );
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,9 @@
-import { writeCookie } from '../common'
-const User = require('../user/model')
-// Login user 
+// Login user
+import {writeCookie} from  "../common"
 
 export const loginUser = async (username, email, password, newUser) => {
     try {
-        const response = await fetch ("http://localhost:5002/users/login", {
+        const response = await fetch("http://localhost:5002/users/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -14,46 +13,52 @@ export const loginUser = async (username, email, password, newUser) => {
                 "email": email,
                 "password": password
             })
-            
         })
         const data = await response.json()
         console.log(data)
         newUser(data.user.username)
 
         writeCookie("jwt_token", data.user.token, 7)
-        
+
     } catch (error) {
         console.log(error)
     }
 }
 
-export const authCheck = async (jwt_token) => {
-    try {   
-        const response = await fetch("http://localhost:5002/users/auth", {
+export const registerUser = async (username, email, password) => {
+    try {
+        const response = await fetch("http://localhost:5002/users/register", {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify({
+                "username" : username,
+                "email" : email,
+                "password": password
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const authCheck = async (jwtToken) => {
+    try {
+        const response = await fetch("http://localhost:5002/users/authCheck", {
             method: "GET",
             headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${jwt_token}`
+                "Content-Type" : "application/json",
+                "Authorization": `Bearer ${jwtToken}`
             }
         })
         const data = await response.json()
         console.log(data)
         return data.user.username
+
     } catch (error) {
         console.log(error)
     }
 }
-
-// Thursday 27.04.2023 // wrong file needs to go into the API
-
-const comparePass = async (req, res, next) => {
-    try {
-        console.log(req.body)
-        let findUser = await User.findOne({where: {username: req.body.username}})
-
-        console.log(password)
-
-    } catch (error) {
-        res.status(501).json({message: error.message, error: error})
-    }
-} 
